e3885d633dee7539328283dfbd40e4228501cf84
shauryajindal <shauryajindal2128@gmail.com>
2026-09-16 02:01:22 +0530

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-   API_BASE_URL :"http://localhost:5003/api",
-  SOCKET_URL: "http://localhost:5003"
+   API_BASE_URL :" https://meetly-9flw.onrender.com/api",
+  SOCKET_URL: " https://meetly-9flw.onrender.com"
 };
