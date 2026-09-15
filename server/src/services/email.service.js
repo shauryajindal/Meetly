a60ff4754580baab6015de1d455aef4719d8ba9d
shauryajindal <shauryajindal2128@gmail.com>
2026-09-16 00:56:52 +0530

    Update email.service.js

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -6,6 +6,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   secure: process.env.SMTP_SECURE === "true",
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD
